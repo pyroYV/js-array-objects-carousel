2aad7e6c0a62407dacaccace2b2f29efd653fcec
pyroYV <simonerizzo.pvt@gmail.com>
2022-06-29 21:12:18 +0200

    bonus 3 ma non 2

--- script.js
+++ script.js
@@ -16,8 +16,7 @@
 // BONUS 2:
 // Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi) l’immagine attiva dovrà cambiare alla successiva.
 // BONUS 3: Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.
-const images = [
-    {
+const images = [{
         url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
         title: 'Svezia',
         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
@@ -53,55 +52,86 @@ console.log(divlist)
 carouselIndex = 0
 
 images.forEach((element) => {
-    // creo un wrapper per le immagini ed eseguo le funzioni per popolarlo, poi lo appendo
+// creo un wrapper per le immagini ed eseguo le funzioni per popolarlo, poi lo appendo
     const imgWrapper = document.createElement('div')
     const newImg = createImg(element)
     const newDiv = createInfo(element)
     imgWrapper.append(newImg)
     imgWrapper.append(newDiv)
     wrapper.append(imgWrapper)
-    
-});
-    //metto la classe active al primo info div
-    divlist[carouselIndex].classList.add('active')
 
-    // funzione per creare un immagine e settare le tag
-function createImg(element){
+});
+//metto la classe active al primo info div
+divlist[carouselIndex].classList.add('active')
+
+// funzione per creare un immagine e settare le tag
+function createImg(element) {
     const newImg = document.createElement('img')
-    newImg.classList.add('w-100','fix')
+    newImg.classList.add('w-100', 'fix')
     newImg.setAttribute('src', element.url)
 
     return newImg
 }
-    // funzione per creare un div in cui sono contenute le informazioni 
-function createInfo(element){
+// funzione per creare un div in cui sono contenute le informazioni 
+function createInfo(element) {
     const newDiv = document.createElement('div')
-    newDiv.classList.add('position-fix','text-end')
-    newDiv.innerHTML = `<h2>${element.title}</h2> <p>${element.description}`
+    newDiv.classList.add('position-fix','text-end','p-3')
+    newDiv.innerHTML = `<h2>${element.title}</h2> <p>${element.description}</p>`
     return newDiv
 }
-    // prendo i due buttons
+// prendo i due buttons
 const nextButton = document.getElementById('next');
 const backButton = document.getElementById('back');
 
 nextButton.addEventListener('click', next)
 backButton.addEventListener('click', back)
 
-function next(){
+// funzione per il pulsante next
+function next() {
     divlist[carouselIndex].classList.remove('active');
     carouselIndex++;
-        if(carouselIndex === images.length){
-            carouselIndex = 0;
-        }
-        divlist[carouselIndex].classList.add('active');
+    if (carouselIndex === images.length) {
+        carouselIndex = 0;
     }
-    
+    divlist[carouselIndex].classList.add('active');
+}
 
-function back(){
+// funzione per il pulsante back
+function back() {
     divlist[carouselIndex].classList.remove('active');
-     carouselIndex--;
-         if(carouselIndex === -1 ){
-             carouselIndex = images.length-1;
-         }
-         divlist[carouselIndex].classList.add('active');
- }
+    carouselIndex--;
+    if (carouselIndex === -1) {
+        carouselIndex = images.length - 1;
+    }
+    divlist[carouselIndex].classList.add('active');
+}
+
+// prendo i bottoni per il tempo
+const startStopButton = document.getElementById('start-stop')
+const reverseButton = document.getElementById('reverse')
+
+let forward = true
+let backgroundInterval = setInterval(forwardBackward, 3000);
+
+function forwardBackward(){
+    if(forward){
+        next()
+    } else{
+        back()
+    }
+}
+
+startStopButton.addEventListener('click',function(){
+    console.log('try')
+    if(backgroundInterval == null){
+        backgroundInterval = setInterval(forwardBackward,3000)
+    } else{
+        clearInterval(backgroundInterval)
+        backgroundInterval = null
+    }
+})
+
+reverseButton.addEventListener('click',function(){
+    forward = !forward
+})
+
